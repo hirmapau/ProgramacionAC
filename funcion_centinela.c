ca533488d35205a1a510bae99069db036cda9469
PaulinAristaHiram <53793948+PaulinAristaHiram@users.noreply.github.com>
2019-09-18 10:01:07 -0500

    Add files via upload

---
+++ funcion_centinela.c
@@ -0,0 +1,37 @@
+/*
+Se requiere un valor centinela como ultimo dato a 
+para manejar los elementos a evaluar y que el bucle 
+no entre en un ciclo infinito.
+*/
+
+
+#include <stdio.h>
+
+
+
+int main(){
+
+const int centinela = -1;
+int nota, cuenta, suma=0;
+
+printf("\vPara salir escriba: -1\n\v");
+printf("\vintroduzca primera calificacion ");
+scanf("%d", &nota);
+
+while (nota != centinela)
+{
+ 	cuenta++;//asigna valor y luego suma por el ++
+	suma += nota;
+	printf("Introduzca la siguiente calificacion ");
+	scanf("%d", &nota);
+
+if(nota == centinela){
+printf("Introdujo %i datos. La suma total es = %i.\n", cuenta, suma);
+	}
+}
+
+puts("final");
+
+
+return 0;
+}
