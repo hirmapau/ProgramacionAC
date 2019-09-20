c6c535a17ac21716adbb8bbe55e3c82364391b64
PaulinAristaHiram <53793948+PaulinAristaHiram@users.noreply.github.com>
2019-09-19 20:29:07 -0500

    Add files via upload

---
+++ while_loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	int index = 1;
+		while (index <= 5) { //ill keep looping through the code WHILE index is less or equal to 5
+			printf("%i\n", index);
+			index++;
+		}
+		return 0;
+}
