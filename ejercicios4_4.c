e06077d5514f5a659e4f3aadb86709dc8d9fa5a7
PaulinAristaHiram <53793948+PaulinAristaHiram@users.noreply.github.com>
2019-09-30 21:25:27 -0500

    Add files via upload

---
+++ ejercicios4_4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void) {
+	int n;
+
+	for (n = 1; n <= 1; n++) { // si es verdadera la segunda parte, entonces el codigo se actualiza a partir de la tercera parte
+		printf("*   *  *  *****  ***** *      *\n");
+		printf("*   *  *  *  *   *   * * *  * *\n");
+		printf("*****  *  * *    ***** *  *   *\n");
+		printf("*   *  *  * *    *   * *      *\n");
+		printf("*   *  *  *  *   *   * *      *\n\n\n");
+
+	}
+
+	return 0;
+}
